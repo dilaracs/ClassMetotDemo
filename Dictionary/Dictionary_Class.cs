5934f9cdc461f3bbe4777689eb6b96485fc05f0d
dilara <76774168+dilaracs@users.noreply.github.com>
2021-01-23 18:01:13 +0300

    Add files via upload

---
+++ Dictionary/Dictionary_Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Dictionary
+{
+    class Dictionary_Class<TKey, TValue>
+    {
+        public TKey[] keys;
+        public TValue[] values;
+
+        public Dictionary_Class()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary_Class<string, string> dictionary = new Dictionary_Class<string, string> { };
+            dictionary.Add("1", "Demir");
+            dictionary.Add("2", "Doruk");
+            dictionary.Add("3", "Acar");
+
+            for (int i = 0; i < dictionary.keys.Length; i++)
+            {
+                Console.WriteLine(dictionary.keys[i].ToString() + " : " + dictionary.values[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
